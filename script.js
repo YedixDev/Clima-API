5a52253e4f2a5c8cc70b0e9e770591f515b3b70a
YedixonRDev <yedixonjrf@gmail.com>
2024-09-27 19:23:35 -0500

    Desarrollando una pagina del climna

---
+++ script.js
@@ -0,0 +1,18 @@
+//Selectores
+
+const content = document.querySelector('.content')
+const formulario = document.querySelector('#formulario')
+const resultado = document.querySelector('#resultado')
+
+window.addEventListener('load', () => {
+  formulario.addEventListener('submit', buscarClima);
+})
+
+function buscarClima(e) {
+  e.preventDefault();
+
+  //Validar
+   
+
+  //consultar
+}
